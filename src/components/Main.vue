7bc09dc408b6c23b112f91169f4eb5ba1ba8d293
StefanoCeli <stefanopsn91@gmail.com>
2021-05-26 16:07:48 +0200

    strutturo chiamata axios

---
+++ src/components/Main.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+      <Search
+      @searchMovie= "this.queryGen"
+     />
+     <h1>{{ this.query }}</h1>
+  </main>
+</template>
+
+<script>
+import Search from './Search';
+import axios from 'axios';
+
+export default {
+    name:'Main',
+    components:{
+        Search
+    },
+    data(){
+        return{
+        apiUrl:"https://api.themoviedb.org/3/search/movie",
+        apiKey:"4df959eab3283b1ac2c5a67b1e5247b9",
+        query:"",
+        language:"it-IT"
+        }
+    },
+    methods:{
+        queryGen(query){
+            this.query = query;
+            axios.get(this.apiUrl,{
+                params:{
+                    apiKey: this.apiKey,
+                    query: this.query,
+                    language: this.language
+                }
+            })
+            .then(resp => {
+                console.log(resp);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+    <form>
+        <input type="text"
+        v-model.trim= "findMovie"
+        >
+        <button
+        @click.prevent="$emit('searchMovie',findMovie)"
+        >Cerca</button>
+    </form>
+</template>
+
+<script>
+export default {
+    name:'Search',
+    data(){
+        return {
+            findMovie: ""
+        }
+    },
+    computed:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
