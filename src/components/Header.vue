0521775f6f905cc1bf36ef0b9a55d5743fd750ee
StefanoCeli <stefanopsn91@gmail.com>
2021-05-28 03:24:09 +0200

    eliminato form

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,38 +1,37 @@
 <template>
     <header>
-        <form>
-            <!-- barra di ricerca -->
-            <div>
-                <input type="text"
-                v-model.trim= "findMovie"
-                placeholder="Ricerca film e serie tv"
-                >
-            </div>
-            <!-- bottoni -->
-            <div>
-                <button
-                @click.prevent="$emit('searchMovie',{text:findMovie, type:'movie'})"
-                >Cerca film</button>
-            </div>
+        
+        <!-- barra di ricerca -->
+        <div>
+            <input type="text"
+             v-model.trim= "findMovie"
+             placeholder="Ricerca film e serie tv"
+            >
+        </div>
+        <!-- bottoni -->
+        <div>
+            <button
+            @click.prevent="$emit('searchMovie',{text:findMovie, type:'movie'})"
+            >Cerca film</button>
+        </div>
 
-            <div>
-                <button
-                @click.prevent="$emit('searchMovie',{text:findMovie, type:'tv'})"
-                >Cerca serie tv</button>
-            </div>
+        <div>
+            <button
+            @click.prevent="$emit('searchMovie',{text:findMovie, type:'tv'})"
+            >Cerca serie tv</button>
+        </div>
 
-            <div>
-                <button
-                @click.prevent="$emit('searchMovie',{text:findMovie, type:'all'})"
-                >Cerca tutto</button>
-            </div>
-            <div>
-                <button
-                @click.prevent="reset"
-                >Reset</button>
-            </div>
+        <div>
+            <button
+            @click.prevent="$emit('searchMovie',{text:findMovie, type:'all'})"
+            >Cerca tutto</button>
+        </div>
+        <div>
+            <button
+            @click.prevent="reset"
+            >Reset</button>
+        </div>
 
-        </form>
     </header>
 </template>
 
